344495aab4a01563dd97e3b267e51a518e1a12d6
DzmitryProkharau <33517308+DzmitryProkharau@users.noreply.github.com>
2018-03-10 20:11:39 +0300

    temp example

--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue;
   }
 
   add(number) {
-    // your implementation
+    this.initialValue = this.initialValue + '+' + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.initialValue = this.initialValue + '-' + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.initialValue = this.initialValue + '*' + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.initialValue = this.initialValue + '/' + number;
+    return this;
+
   }
 
   pow(number) {
-    // your implementation
+    this.initialValue = this.initialValue + '**' + number;
+    return this;
+  }
+
+  valueOf() {
+    var sum = eval(this.initialValue);
+    return sum;
   }
 }
 
